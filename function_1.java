a6691ae2e34419658d37cf720a01fc749e97bcc2
Manas Singh <122530839+ManasSingh77@users.noreply.github.com>
2023-09-13 15:14:03 +0530

    Add files via upload

---
+++ function_1.java
@@ -0,0 +1,14 @@
+package functions;
+
+public class function_1 {
+
+	public static void main(String[] args) {
+		System.out.println("Hey");
+		add();
+	}
+	public static void add() {
+		int a=9;
+		int b=7;
+		System.out.println(a+b);
+	}
+}
