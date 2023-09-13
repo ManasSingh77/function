55f9639f0742d42694cb3041c2b18e772c2b2b28
Manas Singh <122530839+ManasSingh77@users.noreply.github.com>
2023-09-13 15:16:32 +0530

    Update function_1.java

--- function_1.java
+++ function_1.java
@@ -5,10 +5,16 @@ public class function_1 {
 	public static void main(String[] args) {
 		System.out.println("Hey");
 		add();
+		sub();
 	}
 	public static void add() {
 		int a=9;
 		int b=7;
 		System.out.println(a+b);
+	}
+	public static void sub() {
+		int a=9;
+		int b=7;
+		System.out.println(a-b);
 	}
 }
